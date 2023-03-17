e615a6cb88a249a09b66a07682fbe40eb2809f9c
LeivaDiego <lei21752@uvg.edu.gt>
2023-03-17 02:22:31 -0600

    Version 4.0 Variable
    Instancia de variables con sus respectivos getters.

--- Lisp Interpreter/src/VariableHandler.java
+++ Lisp Interpreter/src/VariableHandler.java
@@ -1,2 +1,45 @@
+/**
+ * @author diego leiva, pablo orellana
+ *
+ * Clase encargada del manejo de instancia de variables dentro del programa Lisp
+ */
 public class VariableHandler {
+    private String var_name; // almacena el nombre de la variable
+    private int var_value; // almacena el valor de la variable
+
+    /**
+     * Crea una nueva variable con los respectivos parametros
+     * @param name nombre de la variable
+     * @param value valor de la variable
+     */
+    public VariableHandler(String name, int value){
+        this.var_name = name;
+        this.var_value = value;
+    }
+
+    /**
+     * Obtiene el nombre de una variable dada
+     * @return el nombre de la variable
+     */
+    public String getVar_name() {
+        return var_name;
+    }
+
+    /**
+     * Obtiene el valor de una variable dada
+     * @return el valor de la variable
+     */
+    public int getVar_value() {
+        return var_value;
+    }
+
+    /**
+     * Muestra en pantalla la instancia de la variable creada
+     * @return la informacion de la variable
+     */
+    @Override
+    public String toString(){
+        return this.var_name + ": " +this.var_value;
+    }
+
 }
