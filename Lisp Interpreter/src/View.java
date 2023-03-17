745f0a0297abb56cb999d871bf774127437b925d
LeivaDiego <lei21752@uvg.edu.gt>
2023-03-17 02:14:31 -0600

    Version 3.0 View
    Muestra de mensaje en la interfaz de usuarios

--- Lisp Interpreter/src/View.java
+++ Lisp Interpreter/src/View.java
@@ -1,2 +1,24 @@
+/**
+ * @author diego leiva, pablo orellana
+ *
+ * Clase encargada de mostrar mensajes en pantalla
+ */
 public class View {
+
+    /**
+     * Muestra el mensaje de bienvenida en pantalla
+     */
+    public void Welcome(){
+        System.out.println("Bienvendio al Interprete de Lisp\n");
+    }
+
+    /**
+     * Muestra un mensaje en pantalla
+     * @param str el mensaje a mostrar
+     */
+    public void ShowOnScreen(String str){
+        System.out.println(str+'\n');
+    }
+
+
 }
