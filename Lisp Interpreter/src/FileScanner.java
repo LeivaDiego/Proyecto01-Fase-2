ed007640965146dd19d58cea7bc8a460aa767f10
LeivaDiego <lei21752@uvg.edu.gt>
2023-03-17 01:49:08 -0600

    Version 2.0 File Scanner
    Lectura de archivo y obtencion de expresiones Lisp para su posterior procesamiento.
    
    JUnit test aprobado
    
    EXTRA: archivo de prueba test.txt agregado

--- Lisp Interpreter/src/FileScanner.java
+++ Lisp Interpreter/src/FileScanner.java
@@ -1,2 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileScanner {
+
+    public static ArrayList<String> parse(String filepath) {
+        ArrayList<String> commands = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            String command = "";
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (line.startsWith(";") || line.isEmpty()) {
+                    continue;
+                }
+
+                command += line;
+
+                if (countParentheses(command) == 0) {
+                    commands.add(command.replaceAll("\\s", ""));
+                    command = "";
+                }
+            }
+
+            if (!command.isEmpty()) {
+                throw new RuntimeException("ERROR: Falta un parentesis de cierre");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el archivo: " + filepath);
+        }
+
+        return commands;
+    }
+
+    private static int countParentheses(String command) {
+        int count = 0;
+
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == '(') {
+                count++;
+            } else if (command.charAt(i) == ')') {
+                count--;
+            }
+        }
+
+        return count;
+    }
+
 }
---
+++ Lisp Interpreter/src/FileScannerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileScannerTest {
+
+
+    @Test
+    public void testLispParser() {
+        String filename = "C:/Users/diego/Documents/UVG/5to Semestre/Algoritmos y Estructuras de Datos/Proyecto01-Fase-2/Lisp Interpreter/src/test.txt";
+        ArrayList<String> expectedCommands = new ArrayList<>();
+        expectedCommands.add("(define(squarex)(*xx))");
+        expectedCommands.add("(define(sumxy)(+xy))");
+        expectedCommands.add("(square5)");
+        expectedCommands.add("(sum34)");
+        ArrayList<String> actualCommands = FileScanner.parse(filename);
+        assertEquals(expectedCommands, actualCommands);
+    }
+
+}
